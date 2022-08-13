d64529090eca1b77e658ada072f991a64a2aebca
Yoshihiro Tanaka <contact@cordea.jp>
2022-08-13 14:12:40 +1000

    add a scene

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import { Scene } from 'three';
+import {Color, Group, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
 
+const container = document.getElementById('container');
 const scene = new Scene();
+scene.background = new Color(0xbdbdbd);
+const group = new Group();
+scene.add(group);
+
+const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight);
+
+const renderer = new WebGLRenderer({antialias: true});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
+
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+});
+
+renderer.render(scene, camera);
