ef5c9e65dd1289d453890e060e04d2621e87d5ff
Yoshihiro Tanaka <contact@cordea.jp>
2022-08-13 15:19:40 +1000

    draw text

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import {Color, Group, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+import {Color, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+import {TTFLoader} from "three/examples/jsm/loaders/TTFLoader.js";
+import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry.js";
+import {Font} from "three/examples/jsm/loaders/FontLoader.js";
 
 const container = document.getElementById('container');
 const scene = new Scene();
@@ -7,6 +10,7 @@ const group = new Group();
 scene.add(group);
 
 const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight);
+camera.position.set(0, 0, 1000);
 
 const renderer = new WebGLRenderer({antialias: true});
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -20,4 +24,25 @@ window.addEventListener('resize', function () {
     renderer.render(scene, camera);
 });
 
-renderer.render(scene, camera);
+const loader = new TTFLoader();
+loader.load('font.ttf', function (json) {
+    const font = new Font(json);
+    const text = new TextGeometry('Hello World', {
+        font: font,
+        size: 70,
+        height: 10
+    });
+    text.computeBoundingBox();
+    text.computeVertexNormals();
+
+    const center = -0.5 * (text.boundingBox.max.x - text.boundingBox.min.x);
+    const material = new MeshBasicMaterial({
+        color: 0xffffff
+    });
+    const mesh = new Mesh(text, material);
+    mesh.position.x = center;
+    mesh.rotation.y = Math.PI / 6;
+    group.add(mesh);
+
+    renderer.render(scene, camera);
+});
